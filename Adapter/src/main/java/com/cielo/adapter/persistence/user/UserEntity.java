2e963a12272981d7c242329093eb2912bdffe391
CieloPaez1 <cpaezcampillay@alumnos.undec.edu.ar>
2026-02-05 18:37:21 -0300

    entitys

--- Adapter/src/main/java/com/cielo/adapter/persistence/user/UserEntity.java
+++ Adapter/src/main/java/com/cielo/adapter/persistence/user/UserEntity.java
@@ -1,4 +1,61 @@
 package com.cielo.adapter.persistence.user;
 
+import enums.UserStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    private String activationCode;
+
+    private LocalDateTime activationExpiresAt;
+
+    private LocalDateTime createdAt;
+
+    public UserEntity() {
+    }
+    public UserEntity(Long id, String email, String password, UserStatus status, String activationCode, LocalDateTime activationExpiresAt, LocalDateTime createdAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+        this.activationCode = activationCode;
+        this.activationExpiresAt = activationExpiresAt;
+        this.createdAt = createdAt;
+    }
+    public Long getId() { return id; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public UserStatus getStatus() { return status; }
+    public String getActivationCode() { return activationCode; }
+    public LocalDateTime getActivationExpiresAt() { return activationExpiresAt; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setStatus(UserStatus status) { this.status = status; }
+    public void setActivationCode(String activationCode) { this.activationCode = activationCode; }
+    public void setActivationExpiresAt(LocalDateTime activationExpiresAt) {
+        this.activationExpiresAt = activationExpiresAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
 }
